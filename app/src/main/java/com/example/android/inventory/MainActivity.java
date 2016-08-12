a0b63c16c2d4f31083667e0af90845b0843545ed
Rushi Patel <rushi.m.patel92@gmail.com>
2016-08-12 15:14:44 -0700

    Applied changes

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.android.inventory;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,21 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         DbHelper handler = new DbHelper(this);
         SQLiteDatabase db = handler.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT  * FROM " + ItemReaderContract.ItemEntry.TABLE_NAME
+        final Cursor cursor = db.rawQuery("SELECT  * FROM " + ItemReaderContract.ItemEntry.TABLE_NAME
                 , null);
 
         ListView listView = (ListView) findViewById(R.id.inventory_list);
-        ItemCursorAdapter adapter = new ItemCursorAdapter(this, cursor);
+        final ItemCursorAdapter adapter = new ItemCursorAdapter(this, cursor);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String URL = "content://com.example.android.inventory/items";
-                Uri items = Uri.parse(URL);
-
-                Intent detailIntent = new Intent(Intent.ACTION_VIEW, items);
-                detailIntent.setClass(MainActivity.this, ItemDetail.class);
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent detailIntent = new Intent(MainActivity.this, ItemDetail.class);
+                detailIntent.putExtra("_id", id);
                 startActivity(detailIntent);
             }
         });
